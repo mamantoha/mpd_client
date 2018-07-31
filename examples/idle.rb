491cc79dc614c646f39e6eda2fee2ef4948a3380
Anton Maminov <anton.linux@gmail.com>
2018-07-31 15:03:32 +0300

    add idle example

---
+++ examples/idle.rb
@@ -0,0 +1,25 @@
+require 'bundler'
+Bundler.setup :default
+
+require 'logger'
+require 'mpd_client'
+
+client = MPD::Client.new
+
+client = MPD::Client.new
+client.log = Logger.new($stderr)
+
+client.connect('localhost', 6600)
+
+# Lists all changed systems:
+# database, update, stored_playlist, playlist, player, mixer, output, options, sticker, subscription, message
+#
+subsystems = ['player', 'playlist']
+
+loop do
+  resp = client.idle(*subsystems)
+  puts resp
+end
+
+client.close
+client.disconnect
